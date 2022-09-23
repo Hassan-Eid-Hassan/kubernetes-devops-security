5d97eb694fd222b939618bea19692bb095ace3cb
Hassan Eid <105084413+Hassan-Eid-Hassan@users.noreply.github.com>
2022-09-24 00:26:18 +0200

    Update NumericApplicationTests.java

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -46,9 +46,14 @@ public class NumericApplicationTests {
                 .andExpect(content().string("Greater than 50"));
     }
     
-    @Test
-    public void welcomeMessage() throws Exception {
-         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
+    //@Test
+    //public void welcomeMessage() throws Exception {
+    //     this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
+    //}
+    
+    @Test 
+    public void welcomeMessage() throws Exception { 
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isok()).andExpect(content().string("Kubernetes DevSecOps"));
     }
     
 
